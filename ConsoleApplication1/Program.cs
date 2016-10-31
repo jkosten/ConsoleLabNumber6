94d3bc816a275c4c4480fa6bdf88ad3ba049a4f0
Joe <joekosten@gmail.com>
2016-10-31 16:00:02 -0400

    Current status of Lab 6. Pig Latin Translator will successfully translate all the words in a sentence separately.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Pig Latin Translator!" + Environment.NewLine + Environment.NewLine);
+            Console.WriteLine("Enter a sentence to convert to PigLatin:");
+            string sentence = Console.ReadLine();
+            string pigLatin = ToPigLatin(sentence);
+            Console.WriteLine(pigLatin);
+        }
+
+        static string ToPigLatin(string sentence)
+        {
+            string firstLetter,
+                   restOfWord,
+                   vowels = "AEIOUaeiou";
+            int currentLetter;
+
+            foreach (string word in sentence.Split())
+            {
+                firstLetter = sentence.Substring(0, 1);
+                restOfWord = sentence.Substring(1, sentence.Length - 1);
+                currentLetter = vowels.IndexOf(firstLetter);
+
+                if (currentLetter == -1)
+                {
+                    sentence = restOfWord + firstLetter + "ay";
+                }
+                else
+                {
+                    sentence = word + "way";
+                }
+            }
+            return sentence;
+
+
+        }
+    }
+}
+
--- ConsoleLabNumber6/Program.cs
+++ ConsoleLabNumber6/Program.cs
@@ -10,6 +10,45 @@ namespace ConsoleLabNumber6
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to the Pig Latin Translator!");
+            Console.WriteLine(Environment.NewLine + Environment.NewLine);
+            Console.Write("Please enter a line (in English) to be translated:");
+            string input = Console.ReadLine();
+            string vowels = "aeiou";
+
+            string[] words = input.Split(' ');
+
+            foreach (string word in words)
+            {
+
+                if (IsFirstLetterVowel(word))
+                    Console.Write(word + "way" + " ");
+
+                else
+                {
+                    int vowelPosition = word.IndexOfAny(vowels.ToCharArray());
+                    if (vowelPosition == -1)
+                    {
+                        Console.Write(word + "ay" + " ");
+                    }
+                    else
+                    {
+                        string beforeVowel = word.Substring(0, vowelPosition);
+                        string afterVowel = word.Substring(vowelPosition);
+
+                        Console.Write(afterVowel + beforeVowel + "ay" + " ");
+                    }
+                }
+            }
+        }
+        public static bool IsFirstLetterVowel(string word)
+        {
+            word = word.ToLower();
+            if (word[0] == 'a' || word[0] == 'e' || word[0] == 'i' || word[0] == 'o' || word[0] == 'u')
+            { return true; }
+
+            else
+            { return false; }
         }
     }
 }
